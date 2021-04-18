3c8d9dbbdb77ce78147a72872ba0e4bc6c2e0a0a
Klim Raykov <47781899+EMWD@users.noreply.github.com>
2021-04-18 19:06:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import cowsay7
+import cowsay6
 print(1)
-
+print(2)
 import cowsay2
 import cowsay
 import cowsay
 import cowsay
 import cowsay
 import cowsay2
-import cowsay7
+import cowsay6
+print(2)
